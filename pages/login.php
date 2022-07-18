5ea1e42f06b363ec761c166f0eabf43aa67c48ce
Pablo Conte Corrêa <20191inf0390@AD.SPG.IFSUL.EDU.BR>
2022-07-18 07:52:27 -0300

    test

---
+++ pages/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hellow World Caralho</h1>
+    <div>
+        <h2>Login</h2>
+        <form>
+            <input type="email" placeholder="Digite seu email">
+            <input type="password" placeholder="Digite seu email">
+            <input type="button" value="Corno">
+        </form>
+    </div>
+</body>
+</html>
