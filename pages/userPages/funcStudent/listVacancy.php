23e8d7e1292a2f0bf653274a35aa2485a23b9318
Pablo Conte Correa <pablo.correa.nr@gmail.com>
2022-08-22 08:56:13 -0300

    Change List Vacancy

--- pages/userPages/funcStudent/listVacancy.php
+++ pages/userPages/funcStudent/listVacancy.php
@@ -10,41 +10,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .vaga {
+            border: 0.25em solid #000;
+            padding: 10px;
+            display: flex;
+            justify-content: center;
+            width: 400px;
+            flex-direction: column;
+        }
+    </style>
 </head>
 <body>
+    
     <h1>Listar vagas</h1>
     
-    <table>
-        
-        <thead>
-            <tr>
-                <th scope="col">id_vaga</th>
-                <th scope="col">Salário</th>
-                <th scope="col">Curso</th>
-                <th scope="col">Cargo</th>
-                <th scope="col">Descrição</th>
-                <th scope="col">id_emp</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php 
-                $query = $conn->prepare("SELECT * FROM vaga");
-                $query->execute();
+    <?php 
+        $query = $conn->prepare("SELECT * FROM vaga");
+        $query->execute();
 
-                //var_dump($results);
-                
-                while ($results = $query->fetch(PDO::FETCH_ASSOC)){
-                    echo "<tr>";
-                    echo "<td>".$results["id_vaga"]."</td>";
-                    echo "<td>".$results["salario"]."</td>";
-                    echo "<td>".$results["curso"]."</td>";
-                    echo "<td>".$results["cargo"]."</td>";
-                    echo "<td>".$results["descricao"]."</td>";
-                    echo "<td>".$results["id_emp"]."</td>";
-                }
-            ?>
-        </tbody>
-    </table>
+        //var_dump($results);
+        
+        while ($results = $query->fetch(PDO::FETCH_ASSOC)){
+            echo "<div class='vaga'>";
+            echo "<p>".$results["id_vaga"]."</p>";
+            echo "<p>".$results["salario"]."</p>";
+            echo "<p>".$results["curso"]."</p>";
+            echo "<p>".$results["cargo"]."</p>";
+            echo "<p>".$results["descricao"]."</p>";
+            echo "<p>".$results["id_emp"]."</p>";
+            echo "<button>Registrar interesse</button>";
+            echo "</div>";
+        }
+    ?>
     
     <br><a href="../studentPage.php">companyPage</a>
 </body>
