20f1b94c5ec666a9a4a4d84ed4fe19d4377be676
Pablo-Conte <pablo.correa.nr@gmail.com>
2022-08-29 17:11:22 -0300

    Feat -> Estilização listagem de vagas

--- pages/userPages/funcStudent/listVacancy.php
+++ pages/userPages/funcStudent/listVacancy.php
@@ -12,6 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../../Bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="../../css/default.css">
+    <link rel="stylesheet" href="../../css/listVacancy.css">
     <script src="../../../Bootstrap/js/bootstrap.bundle.js"></script>
     <title>Listar Vagas</title>
 </head>
@@ -21,41 +22,47 @@
 <?php
     require_once "../structure/headerUsers.php";
 ?>
-    <link rel="stylesheet" href="./listVacancy.css">
     
     <div class="main">
         <h1>Listar vagas</h1>
-        
-        <?php
-            $query = $conn->prepare("SELECT * FROM vaga");
-            $query->execute();
-            //var_dump($results);
-            $curso = "";
-            $empresa = "";
-            
-            while ($results = $query->fetch(PDO::FETCH_ASSOC)){
-                if ($results["curso"] == 1){
-                    $curso = "Informática";
-                }
-                if ($results["curso"] == 2){
-                    $curso = "Eletromecânica";
-                }
+        <div class="listar">
+            <?php
+                $query = $conn->prepare("SELECT * FROM vaga");
+                $query->execute();
+                $curso = "";
+                $empresa = "";
                 
-                $query1 = $conn->prepare("SELECT nome FROM empresa WHERE id_empresa = :idempresa");
-                $query1->bindParam(":idempresa", $results['id_emp']);
-                $query1->execute();
-                $empresa = $query1->fetch(PDO::FETCH_ASSOC)['nome'];
+                while ($results = $query->fetch(PDO::FETCH_ASSOC)){
+                    
+                    if ($results["curso"] == 1){
+                        $curso = "Informática";
+                    }
+                    if ($results["curso"] == 2){
+                        $curso = "Eletromecânica";
+                    }
+                    
+                    $query1 = $conn->prepare("SELECT nome FROM empresa WHERE id_empresa = :idempresa");
+                    $query1->bindParam(":idempresa", $results['id_emp']);
+                    $query1->execute();
+                    $empresa = $query1->fetch(PDO::FETCH_ASSOC)['nome'];
 
-                echo "<div class='vaga'>";
-                echo "<p>Cargo: ".$results["cargo"]."</p>";
-                echo "<p>Salário: ".$results["salario"]."</p>";
-                echo "<p>Curso: ".$curso."</p>";
-                echo "<p>Descrição: ".$results["descricao"]."</p>";
-                echo "<p>".$empresa."</p>";
-                echo "<button>Registrar interesse</button>";
-                echo "</div>";
-            }
-        ?>
+                    echo "<div class='vaga'>";
+                    echo "<div class='nomeEmpresa'>".$empresa."</div>";
+                    echo "<div class='corpoVaga'>";
+                    echo "<p>Vaga: ".$results["cargo"]."</p>";
+                    echo "<p>Salário: ".$results["salario"]."</p>";
+                    echo "<p>Curso: ".$curso."</p>";
+                    echo "<p>Descrição: ".$results["descricao"]."</p>";
+                    echo "</div>";
+                    echo "<button>Registrar Interesse</button>";
+                    echo "</div>";
+                    
+                }
+            ?>
+        </div>
     </div>
+    
+
+
 </body>
 </html>
