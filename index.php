9c1e71d575d1f74b9e35366b4047a3bd513415ce
Thomas Schmidt <107225582+Yoss03@users.noreply.github.com>
2022-11-27 22:37:44 -0300

    Atualizações 27/11
    Index quase 100%

--- index.php
+++ index.php
@@ -8,37 +8,54 @@
   <link rel="stylesheet" href="./Bootstrap/css/bootstrap.css">
   <link rel="stylesheet" href="./pages/css/cssindex.css">
   <script src="./Bootstrap/js/bootstrap.bundle.js"></script>
-  <title>Index</title>
+  <title>COPEX Estágios</title>
 </head>
 
-<body>
-  <nav class="navbar navbar-dark bg-dark sticky-top">
+<body class="corfundo">
+  <!-- talvez colocar um daqueles botao de hamburguer q tenhga link pro site do ifsul e o moodle -->
+  <!--NAVBAR AQUI -->
+  <nav class="navbar sticky-top">
     <div class="container-fluid">
-      <a class="navbar-brand ">COPEX</a>
+      <a class="navbar-brand"><b>COPEX Estágios</b></a>
       <form class="d-flex" role="search">
         <a href="./pages/login.php">
-          <div class="btn btn-outline-success">Login</div>
+          <div class="btn btn-outline"><b>Login</b></div>
         </a>
       </form>
     </div>
   </nav> 
-
+  <!--DIV PRINCIPAL-->
   <div class="container11">
     <div>
       <div class="itensimg">
-        <h3>COPEX Estágios</h3>
-        <p>Aqui você poderá conferir as vagas ofertadas aos alunos do IFSul - campus Sapiranga</p>
+        <h1 text-align="center"><b>COPEX Estágios</b></h1>
+        <hr class="hrvisual">
+        <p class="textodivmain">Sejam bem-vin@s.</p>
+        <p class="textodivmain"> Aqui você pode conferir as vagas ofertadas para os alunos do IFSul - campus Sapiranga.</p>
+        <br><br><br><br>
       </div>
     </div>
     <div class="itensimg">
-      <h3>LOGO DA COPEX BEM GRANDÃO AQUI</h3>
-      
+      <h3 >trocar plano de fundo pra degrade ou cor solida e colocar aquela imagem minimalista</h3>
     </div>
-
   </div>
-
-  
-
+  <!-- FOOTER COMEÇA AQUI-->
+  <footer margin-bottom="0%">
+    <div class="containerfooter">
+      <div class="footeritems logoifsul">
+        <img class="logoifsul" src="imagens\Sapiranga\Sapiranga\Marca\Versão-Horizontal-2.png" alt="Logo IFSul">        
+      </div>
+      <div class="footeritems textofooter">
+        <p>Contatos:</p>
+        <p>E-mail COPEX: sg-copex@ifsul.edu.br</p>
+        <p>Whatsapp: (51) 99504-3303</p>
+      </div>
+      <div class="footeritems textofooter">
+        <p>Atendimento:</p>
+        <p>Horário: 10h às 19h</p>
+        <p>Dias da semana: segunda-feira à sexta-feira.</p>
+      </div>
+    </div>
+  </footer>
 </body>
-
 </html>
