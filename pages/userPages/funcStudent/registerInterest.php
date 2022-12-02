ceae43a82cc830314e5b2567db5478350bba202b
Pablo-Conte <pablo.correa.nr@gmail.com>
2022-12-02 00:26:51 -0300

    Feat -> Doing whatsapp message, library added to.

--- pages/userPages/funcStudent/registerInterest.php
+++ pages/userPages/funcStudent/registerInterest.php
@@ -1,100 +1,109 @@
 <?php
+    require_once('../../../vendor/autoload.php');
+    use Twilio\Rest\Client;
+    require_once "../../../includes/connection.php";
+    require_once "./studentAuth.php";
 
-//require_once('../../../vendor/autoload.php');
-//use Twilio\Rest\Client;
-require_once "../../../includes/connection.php";
-require_once "./studentAuth.php";
+    require_once('../../../library/mailSRC/PHPMailer.php');
+    require_once('../../../library/mailSRC/SMTP.php');
+    require_once('../../../library/mailSRC/Exception.php');
 
-require_once('../../../library/mailSRC/PHPMailer.php');
-require_once('../../../library/mailSRC/SMTP.php');
-require_once('../../../library/mailSRC/Exception.php');
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
 
-require_once '../../../library/sms/vendor/autoload.php';
+    //precisamos a partir da outra página, receber o ID da empresa
+    //Assim poderemos pesquisar no banco o e-mail da empresa para colocar como addAddress (para quem vamos enviar um e-mail)
+    //Para colocar os dados do aluno no corpo do e-mail é fácil, basta puxar da sessão
 
+    $nomeAluno = $_SESSION['name'];
+    $numeroAluno = $_SESSION['numero'];
+    $emailAluno = $_SESSION['email'];
+    $idEmpresa = $_POST['idEmpresa'];
+    $cargo = $_POST['cargo'];
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
+    $query = $conn->prepare("SELECT email FROM empresa WHERE id_empresa = $idEmpresa");
+    $query->execute();
 
-//precisamos a partir da outra página, receber o ID da empresa
-//Assim poderemos pesquisar no banco o e-mail da empresa para colocar como addAddress (para quem vamos enviar um e-mail)
-//Para colocar os dados do aluno no corpo do e-mail é fácil, basta puxar da sessão
+    $results = $query->fetch(PDO::FETCH_ASSOC);
+    $emailEmpresa = $results['email'];
 
-$nomeAluno = $_SESSION['name'];
-$numeroAluno = $_SESSION['numero'];
-$emailAluno = $_SESSION['email'];
-$idEmpresa = $_POST['idEmpresa'];
-$cargo = $_POST['cargo'];
+    $mail = new PHPMailer(true);
 
-$query = $conn->prepare("SELECT email FROM empresa WHERE id_empresa = $idEmpresa");
-$query->execute();
+    // try {
+    //     $mail->isSMTP();
+    //     $mail->Host = 'smtp.gmail.com';
+    //     $mail->SMTPAuth = true;
+    //     $mail->Username = 'bobesponjamailer@gmail.com'; //Uma conta com as devidas configs para aceitar requisições desse tipo.
+    //     $mail->Password = '-'; //Necessário pass pública do email usado para enviar os e-mails (bob).
+    //     $mail->Port = 587;
 
-$results = $query->fetch(PDO::FETCH_ASSOC);
-$emailEmpresa = $results['email'];
+    //     $mail->setFrom('bobesponjamailer@gmail.com');
+    //     $mail->addAddress($emailEmpresa);
 
-$mail = new PHPMailer(true);
+    //     $mail->isHTML(true);
+    //     $mail->Subject = "$nomeAluno registou interesse na sua vaga!";
+    //     $mail->Body = "<p>Dados de $nomeAluno:</p>
+    //         <ul>
+    //             <li>Nome: $nomeAluno</li>
+    //             <li>Numero de contato: $numeroAluno</li>
+    //             <li>Email de contato: $emailAluno</li>
+    //             <li>Vaga de interesse: $cargo</li>
+    //         </ul>
+    //         ";
+    //     $mail->AltBody = 'Chegou o email teste de Pablin!!';
 
-try {
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'bobesponjamailer@gmail.com'; //Uma conta com as devidas configs para aceitar requisições desse tipo.
-    $mail->Password = '-'; //Necessário pass pública do email usado para enviar os e-mails (bob).
-    $mail->Port = 587;
+    //     $_SESSION['m'] = "Interesse foi registrado com sucesso!";
 
-    $mail->setFrom('bobesponjamailer@gmail.com');
-    $mail->addAddress($emailEmpresa);
+    //     $mail->send();
 
-    $mail->isHTML(true);
-    $mail->Subject = "$nomeAluno registou interesse na sua vaga!";
-    $mail->Body = "<p>Dados de $nomeAluno:</p>
-        <ul>
-            <li>Nome: $nomeAluno</li>
-            <li>Numero de contato: $numeroAluno</li>
-            <li>Email de contato: $emailAluno</li>
-            <li>Vaga de interesse: $cargo</li>
-        </ul>
-        ";
-    $mail->AltBody = 'Chegou o email teste de Pablin!!';
+    //     header("location: ./listVacancy.php");
+        
+    // } catch (Exception $e) {
+    //     echo "Erro ao enviar mensagem: {$mail->ErrorInfo}";
+    //     //Provavelmente tu estás vendo essa parte do código pq a autenticação não funfou né prof? ahahahha, podemos testar em aulas juntos se quiser ;), ou tu pode criar um email e configurar certin para colocar no $mail->Username e tals
+    // }
 
-    $_SESSION['m'] = "Interesse foi registrado com sucesso!";
+    //Implementação do envio de SMS, em trabalho ainda.
 
-    $mail->send();
+    // try {
+        
+    //     $account_sid = '';
+    //     $auth_token = '';
 
-    header("location: ./listVacancy.php");
-    
-} catch (Exception $e) {
-    echo "Erro ao enviar mensagem: {$mail->ErrorInfo}";
-    //Provavelmente tu estás vendo essa parte do código pq a autenticação não funfou né prof? ahahahha, podemos testar em aulas juntos se quiser ;), ou tu pode criar um email e configurar certin para colocar no $mail->Username e tals
-}
+    //     $twilio_number = "(726) 227-2391 ";
 
+    //     $client = new Client($account_sid, $auth_token);
+    //     $client->messages->create(
+    //             // Where to send a text message (your cell phone?)
+    //             '+5551997602457',
+    //             array(
+    //                 'from' => $twilio_number,
+    //                 'body' => 'Projeto COPEX está funcionando com SMS agora!'
+    //             )
+    //         );
+        
+    // } catch (Exception $e) {
+    //     echo "Erro ao enviar SMS";
+    // }
 
+    try {
 
-
-
-//Implementação do envio de SMS, em trabalho ainda.
-
-// try {
-    
-//     // Your Account SID and Auth Token from twilio.com/console
-//     $account_sid = '-';
-//     $auth_token = '-';
-//     // In production, these should be environment variables. E.g.:
-//     // $auth_token = $_ENV["TWILIO_AUTH_TOKEN"]
-
-//     // A Twilio number you own with SMS capabilities
-//     $twilio_number = "-";
-
-//     $client = new Client($account_sid, $auth_token);
-//     $client->messages->create(
-//         // Where to send a text message (your cell phone?)
-//         '+5551997602457',
-//         array(
-//             'from' => $twilio_number,
-//             'body' => 'Projeto COPEX está funcionando com SMS agora!'
-//         )
-// );
-    
-// } catch (Exception $e) {
-//     echo "Erro ao enviar SMS";
-// }
+        // Find your Account SID and Auth Token at twilio.com/console
+        // and set the environment variables. See http://twil.io/secure
+        $sid = "";
+        $token = "";
+        $twilio = new Client($sid, $token);
+        
+        $message = $twilio->messages
+                          ->create("whatsapp:+5551997602457", // to
+                                   [
+                                       "from" => "whatsapp:+14155238886",
+                                       "body" => "Hi Pablo, it's working very well, good job"
+                                   ]
+                          );
+        
+        print($message->sid);
+    } catch (Exception $e){
+        echo "oh shit, here we go again!\n" . $e;
+    }
 ?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '17b3d3a6981545de0b4c1d2b387b6d4008558a74',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '17b3d3a6981545de0b4c1d2b387b6d4008558a74',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twilio/sdk' => array(
+            'pretty_version' => '6.43.4',
+            'version' => '6.43.4.0',
+            'reference' => '805dd4890602542c98351921f50a664d89f782a0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twilio/sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
